3cbe223fce610ac838b9f70e58b3e64f91119957
유완 <ywan311@naver.com>
2019-04-08 00:16:18 +0900

    final graphql

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
-import { HashRouter as Router, Route } from "react-router-dom"
 import client from "./apolloClient";
 import Home from "./Home";
 import Detail from "./Detail";
 
 class App extends Component {
   render() {
-
-    return <ApolloProvider client={client}>
-      <Router>
-        <React.Fragment>
-          <Route exact={true} path={"/"} component={Home} />
-          <Route path={"/details/:moviesId"} component={Detail} />
-        </React.Fragment>
-      </Router>
-    </ApolloProvider>;
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <main>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:movieId"} component={Detail} />
+          </main>
+        </Router>
+      </ApolloProvider>
+    );
   }
 }
 
--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,8 +1,7 @@
 import ApolloClient from "apollo-boost";
 
-const client = new ApolloClient({
-    url:"localhost:4000"
+const client =new ApolloClient ({
+  uri:"http://localhost:4000/"
 
-})
-
+});
 export default client;
